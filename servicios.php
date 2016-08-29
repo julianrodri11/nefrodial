ad88b5be988c8764dd55b86aeb6178ec716bb217
jerodriv9 <jerodriv9@hotmail.com>
2016-08-29 13:13:39 -0500

    Modificando titulos de servicios

--- servicios.php
+++ servicios.php
@@ -17,19 +17,19 @@
 
 	<div class="container">
 	<div class="row">
-		<div class="col s12 m6 l4"><h5 class="center-align"><i class="material-icons">add</i>Diálisis</h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		<div class="col s12 m6 l4"><h5 class="center-align"><i class="material-icons">add</i>Consulta</h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+		<div class="col s12 m6 l4"><h5 class="center-align"><i class="material-icons">add</i>Prediálisis</h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
 		<div class="col s12 m6 l4"><h5 class="center-align"><i class="material-icons">add</i>Hemodiálisis</h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-		<div class="col s12 m6 l4"><h5 class="center-align"><i class="material-icons">add</i>Diálisis</h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
@@ -37,24 +37,19 @@
 		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
 
 		
-		<div class="col s12 m6 l4"><h5 class="center-align"><i class="material-icons">add</i>Hemodiálisis</h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
 		<div class="col s12 m6 l4"><h5 class="center-align"><i class="material-icons">add</i>Diálisis</h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
-		<div class="col s12 m6 l4"><h5 class="center-align"><i class="material-icons">add</i>Hemodiálisis</h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+		<div class="col s12 m6 l4"><h5 class="center-align"><i class="material-icons">add</i>Transplante Renal</h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 		consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+		
 	</div>
 </div>
 	<?php include_once('footer.php'); ?>
